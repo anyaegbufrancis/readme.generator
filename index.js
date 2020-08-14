f9246de674d2efd07b0bb665b68c9b32d5048afb
Anyaegbu Francis <anyaegbufrancis@gmail.com>
2020-08-14 18:43:50 +0400

    initial validator modules

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
+const {description, emailChecker} = require("./utils/validate");
 const colors = require('colors');
 const { error } = require("console");
 const util = require("util")
