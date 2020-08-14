d2c9d9330c4d8d8b782f5146d1174ddcc721b37f
Anyaegbu Francis <anyaegbufrancis@gmail.com>
2020-08-14 18:59:50 +0400

    validate module

---
+++ utils/validate.js
@@ -0,0 +1,14 @@
+//Declaring Global constants for packages and local validators
+
+const { strikethrough } = require("colors");
+
+const emailValidate = require("email-validator");
+
+
+                                                                               });
+
+
+//Exporting Modules for use in other files
+
+module.exports = {}
+
