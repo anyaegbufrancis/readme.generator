96675ea364aa2b794758f8830051902a7c16d6f8
Anyaegbu Francis <anyaegbufrancis@gmail.com>
2020-08-14 19:01:36 +0400

    validate module

--- utils/validate.js
+++ utils/validate.js
@@ -9,6 +9,7 @@ const githubValidate = require("gh-account-exists");
 //Define interactive data validation checks
 const validateInput = input => input === "" ? "This input is required" : true;
 const description = input => ((input.split('').length) < 50) ? "Please input project description upto 50 characters or more..." : true;
+const emailChecker = email => emailValidate.validate(email) ? true : "Please enter a valid email address...";
 
 
                                                                                });
