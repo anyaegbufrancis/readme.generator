d3c9c43dfd58774bc2ad721a88cb07bf559a91ac
Anyaegbu Francis <anyaegbufrancis@gmail.com>
2020-08-14 18:45:06 +0400

    Added github account validator module

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
-const {description, emailChecker, validateInput} = require("./utils/validate");
+const {description, emailChecker, validateInput, githubAccountValidate} = require("./utils/validate");
 const colors = require('colors');
 const { error } = require("console");
 const util = require("util")
