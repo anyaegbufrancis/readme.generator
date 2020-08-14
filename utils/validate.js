a6145108c721aa54f29e44dd1f829a8b0fc79279
Anyaegbu Francis <anyaegbufrancis@gmail.com>
2020-08-14 19:02:04 +0400

    validate module

--- utils/validate.js
+++ utils/validate.js
@@ -10,8 +10,15 @@ const githubValidate = require("gh-account-exists");
 const validateInput = input => input === "" ? "This input is required" : true;
 const description = input => ((input.split('').length) < 50) ? "Please input project description upto 50 characters or more..." : true;
 const emailChecker = email => emailValidate.validate(email) ? true : "Please enter a valid email address...";
-
-
+const githubAccountValidate = github => githubValidate(github).then(exists => {
+    if (exists === false) {
+        console.log("\nTry a valid github username\n".red)
+        } else {
+            console.log("\nValid Github Username!\n".green)
+            
+            return exists
+            }
+         //console.log(exists)
                                                                                });
 
 
