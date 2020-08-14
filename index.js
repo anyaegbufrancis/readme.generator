fa21fe369b78f9be76728ed293f5f4a5d41955eb
Anyaegbu Francis <anyaegbufrancis@gmail.com>
2020-08-14 18:43:06 +0400

    Adding markdown modules

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Global declarations
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 const colors = require('colors');
 const { error } = require("console");
 const util = require("util")
