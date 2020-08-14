a9f3b5f022b53d7747724af61ecdf0fb2303e302
Anyaegbu Francis <anyaegbufrancis@gmail.com>
2020-08-14 18:42:26 +0400

    Update to Required modules

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 //Global declarations
 const inquirer = require("inquirer");
 const fs = require("fs");
+const colors = require('colors');
+const { error } = require("console");
+const util = require("util")
